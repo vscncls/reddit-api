fc373326fc5d573098924cad1b4594a35e099f15
Lucas Vasconcelos <lucas@vscncls.xyz>
2021-07-11 23:54:23 -0300

    fix post author

--- src/RedditClient.ts
+++ src/RedditClient.ts
@@ -6,7 +6,7 @@ type RedditApiResponse = {
     children: Array<{
       data: {
         title: string;
-        author_fullname: string;
+        author: string;
         created_utc: number;
         ups: number;
         num_comments: number;
@@ -21,7 +21,7 @@ export class RedditClient {
 
     return response.data.data.children.map(({ data: post }) => ({
       title: post.title,
-      author: post.author_fullname,
+      author: post.author,
       createdAt: new Date(post.created_utc * 1000),
       upvotes: post.ups,
       commentsCount: post.num_comments,
